d02aa014d194be99ce574e97677c44ac22842fea
vnpugh <vnp19@yahoo.com>
2023-06-02 04:48:14 -0500

    Setting up Service Layers & Data

--- src/main/java/service/WatchListService.java
+++ src/main/java/service/WatchListService.java
@@ -1,16 +1,47 @@
 package service;
 
+import exceptions.StockNotFoundException;
+import io.cucumber.java.eo.Se;
 import models.Stock;
 import models.User;
 import models.WatchList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.UserRepository;
+import repository.WatchListRepository;
 
 import java.util.List;
 
+@Service
 public class WatchListService {
+    private UserRepository userRepository;
+    private WatchListRepository watchListRepository;
+    private Object stockService;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Autowired
+    public void setWatchListRepository(WatchListRepository watchListRepository) {
+        this.watchListRepository = watchListRepository;
+    }
+
 
 
     public Stock addStockToWatchlist(User user, String symbol) {
+        Stock stock = stockService.getStockBySymbol(symbol);
+
+        if (stock != null) {
+            user.getWatchList().addStock(stock);
+            return stock;
+        } else {
+            throw new StockNotFoundException("Stock not found for symbol: " + symbol);
+        }
     }
+
+
 
     public List<Stock> getAllStocksOnWatchList(User user) {
     }
