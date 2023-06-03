9ca9718802bd5c509a13d015aae7378dea35fdce
vnpugh <vnp19@yahoo.com>
2023-06-03 10:29:27 -0500

    Added security dependency

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,64 +1,70 @@
-//package service;
-//
-//
-//
-//
-//import exceptions.InformationExistException;
-//import models.User;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.context.annotation.Lazy;
-//import org.springframework.stereotype.Service;
-//import repository.UserRepository;
-//
-//
-//
-//@Service
-//public class UserService {
-//
-//    private UserRepository userRepository;
-//
-//    private PasswordEncoder passwordEncoder;
-//
-//    @Autowired
-//    public UserService(UserRepository userRepository,
-//                       @Lazy PasswordEncoder passwordEncoder) {
-//        this.userRepository = userRepository;
-//        this.passwordEncoder = passwordEncoder;
-//
-//    }
-//
-//    /**
-//     * Creates a new user.
-//     * @param userObject The User object containing the details of the user to be created.
-//     * @return The created User object.
-//     * @throws InformationExistException if a user with the same email address already exists.
-//     */
-//    public User createUser(User userObject) throws InformationExistException {
-//        if (!userRepository.existsByEmailAddress(userObject.getEmail())) {
-//            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
-//            return userRepository.save(userObject);
-//        } else {
-//            throw new InformationExistException("User with email address " + userObject.getEmail() +
-//                    " already exists.");
-//        }
-//    }
-//
-//    /**
-//     * Finds a user by their email.
-//     * @param email the email of the user to find
-//     * @return the User object corresponding to the provided email
-//     */
-//    public User findUserByEmail(String email){
-//        return userRepository.findUserByEmail(email);
-//    }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//}
+package service;
+
+import exceptions.InformationExistException;
+import models.User;
+import models.request.LoginRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+import repository.UserRepository;
+
+
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository,
+                       @Lazy PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+
+    }
+
+    /**
+     * Creates a new user.
+     * @param userObject The User object containing the details of the user to be created.
+     * @return The created User object.
+     * @throws InformationExistException if a user with the same email address already exists.
+     */
+    public User createUser(User userObject) throws InformationExistException {
+        if (!userRepository.existsByEmailAddress(userObject.getEmail())) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        } else {
+            throw new InformationExistException("User with email address " + userObject.getEmail() +
+                    " already exists.");
+        }
+    }
+
+    /**
+     * Finds a user by their email.
+     * @param email the email of the user to find
+     * @return the User object corresponding to the provided email
+     */
+    public User findUserByEmail(String email){
+        return userRepository.findUserByEmail(email);
+    }
+
+
+    public User registerUser(User userObject) {
+    }
+
+    public ResponseEntity<?> loginUser(LoginRequest loginRequest) {
+    }
+
+    public User getCurrentUser() {
+    }
+
+    public User updateCurrentUser(User userObject) {
+    }
+
+    public ResponseEntity<?> deleteCurrentUser() {
+    }
+}
--- src/test/java/definitions/LoginStepDefinitions.java
+++ src/test/java/definitions/LoginStepDefinitions.java
@@ -1,13 +1,17 @@
 package definitions;
 
+import com.jayway.jsonpath.DocumentContext;
 import com.stockwatch.capstone.CapstoneApplication;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.Assert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -33,6 +37,22 @@ public class LoginStepDefinitions {
         request.header("Content-Type", "application/json");
         response = request.body(requestBody.toString()).post(BASE_URL + port +"/api/users/register");
     }
+
+    @When("I enter my valid username and password")
+    public void iEnterMyValidUsernameAndPassword() {
+        JsonPath jsonObject = new JsonPath(response.asString());
+        String email = jsonObject.get("data.user.email");  // Adjust the JSON path if needed
+        String password = "password100";
+
+        Assert.assertEquals("email100@gmail.com", email);
+        Assert.assertEquals("password100", password);
+    }
+
+
+
+
+
+
 
 
 }
