a1ec855240ca3b586ecc1e9e183a7a755272fdae
vnpugh <vnp19@yahoo.com>
2023-06-05 08:15:43 -0500

    Updating Industry Model

--- src/main/java/models/Industry.java
+++ src/main/java/models/Industry.java
@@ -21,5 +21,32 @@ public class Industry {
     public Industry() {
     }
 
+    public Industry(Long industryId, String industry) {
+        this.industryId = industryId;
+        this.industry = industry;
+    }
 
+    public Long getIndustryId() {
+        return industryId;
+    }
+
+    public void setIndustryId(Long industryId) {
+        this.industryId = industryId;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    @Override
+    public String toString() {
+        return "Industry{" +
+                "industryId=" + industryId +
+                ", industry='" + industry + '\'' +
+                '}';
+    }
 }
