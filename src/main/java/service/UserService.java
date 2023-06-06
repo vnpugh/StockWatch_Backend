bf8851ca6996bb464cef3235469e7f8c22caec2f
vnpugh <vnp19@yahoo.com>
2023-06-06 15:23:09 -0500

    Updating user service

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -2,17 +2,24 @@ package service;
 
 import exceptions.InformationExistException;
 import exceptions.InformationNotFoundException;
+import exceptions.InvalidInputException;
 import models.User;
 import models.request.LoginRequest;
+import models.request.RegisterUserRequest;
+import models.request.UpdateUserRequest;
+import models.response.LoginResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import repository.UserRepository;
-
-import java.util.Optional;
+import security.JWTUtils;
+import security.MyUserDetails;
 
 
 @Service
@@ -20,81 +27,100 @@ public class UserService {
 
     private UserRepository userRepository;
 
-    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JWTUtils jwtUtils;
+    @Autowired
+    private AuthenticationManager authManager;
 
     @Autowired
-    public UserService(UserRepository userRepository,
-                       @Lazy PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-
-    }
-
-    /**
-     * Creates a new user.
-     * @param userObject The User object containing the details of the user to be created.
-     * @return The created User object.
-     * @throws InformationExistException if a user with the same email address already exists.
-     */
-    public User createUser(User userObject) throws InformationExistException {
-        if (!userRepository.existsByEmail(userObject.getEmail())) {
-            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
-            return userRepository.save(userObject);
-        } else {
-            throw new InformationExistException("User with email address " + userObject.getEmail() +
-                    " already exists.");
-        }
     }
 
     /**
      * Finds a user by their email.
+     *
      * @param email the email of the user to find
      * @return the User object corresponding to the provided email
      */
-    public User findUserByEmail(String email){
-        return userRepository.existByEmail(email);
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 
 
+    /**
+     * To Login user and generate JWT token.
+     *
+     * @param loginRequest
+     * @return LoginResponse
+     */
+    public LoginResponse loginUser(LoginRequest loginRequest) {
+        try {
+            Authentication authentication = authManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginRequest.getEmail(), loginRequest.getPassword())
+            );
 
-    public ResponseEntity<?> loginUser(LoginRequest loginRequest) {
-            String email = loginRequest.getEmail();
-            String password = loginRequest.getPassword();
-
-            // Validate the username and password
-            if (email.equals("email100@gmail.com") && password.equals("password100")) {
-                // Successful login
-                return ResponseEntity.ok("Login successful");
-            } else {
-                // Failed login
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
-            }
+            MyUserDetails user = (MyUserDetails) authentication.getPrincipal();
+            String accessToken = jwtUtils.generateJwtToken(user);
+            return new LoginResponse(user.getUser().getEmail(), accessToken);
+        } catch (Exception e) {
+            throw new InvalidInputException("invalid username/password");
         }
+    }
 
 
-    public User registerUser(User userObject) {
+    /**
+     * Creates a new user.
+     *
+     * @param userObject The User object containing the details of the user to be created.
+     * @return The created User object.
+     * @throws InformationExistException if a user with the same email address already exists.
+     */
+    public User registerUser(RegisterUserRequest userObject) throws InformationExistException {
         if (!userRepository.existsByEmail(userObject.getEmail())) {
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
             String encodedPassword = passwordEncoder.encode(userObject.getPassword());
-            userObject.setPassword(encodedPassword);
-            User registeredUser = userRepository.save(userObject);
-            return registeredUser;
+            User user = new User();
+            user.setEmail(userObject.getEmail());
+            user.setPassword(encodedPassword);
+            user.setFirstName(userObject.getFirstName());
+            return userRepository.save(user);
         } else {
-            throw new RuntimeException("User with the given email already exists");
+            throw new InformationExistException("User with the given email already exists");
         }
     }
 
-    public User getCurrentLoggedInUser(){
-        Optional<User> user = userRepository.findCurrentLoggedInUserById(getCurrentLoggedInUser().getId());
-        if (user.isPresent()) {
-            return user.get();
-        } else throw new InformationNotFoundException("User with Id " + getCurrentLoggedInUser().getId() + " does not exist.");
+    /**
+     * Get current logged-in user for whom jwt is valid.
+     *
+     * @return User logged-in
+     */
+    public User getCurrentLoggedInUser() {
+        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (userDetails != null) {
+            return userDetails.getUser();
+        } else throw new InformationNotFoundException("User not logged in");
     }
 
-    public User updateCurrentUser(User userObject) throws InformationNotFoundException{
+    /**
+     * Update info of logged-in user
+     *
+     * @param updateUserRequest
+     * @return Updated user
+     * @throws InformationNotFoundException
+     */
+    public User updateCurrentUser(UpdateUserRequest updateUserRequest) throws InformationNotFoundException {
         User updatedUser = getCurrentLoggedInUser();
-        updatedUser.setEmail(getCurrentLoggedInUser().getEmail());
-        updatedUser.setPassword(getCurrentLoggedInUser().getPassword());
-        return userRepository.save(updatedUser);
+        if (updatedUser != null) {
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            String encodedPassword = passwordEncoder.encode(updateUserRequest.getPassword());
+            updatedUser.setEmail(updateUserRequest.getEmail());
+            updatedUser.setPassword(encodedPassword);
+            return userRepository.save(updatedUser);
+        } else {
+            throw new InformationNotFoundException("User not logged-in");
+        }
     }
 
     public ResponseEntity<?> deleteCurrentUser() {
