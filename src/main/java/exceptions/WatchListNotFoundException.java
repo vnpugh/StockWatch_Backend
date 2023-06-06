e1ce345ff005ebb1e1a6fcdfbeb81cbd7ad137ff
vnpugh <vnp19@yahoo.com>
2023-06-06 14:16:02 -0500

    Added watchlist & invalidinput exception

---
+++ src/main/java/exceptions/WatchListNotFoundException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class WatchListNotFoundException extends RuntimeException {
+    public WatchListNotFoundException(String message) {
+        super(message);
+    }
+}
