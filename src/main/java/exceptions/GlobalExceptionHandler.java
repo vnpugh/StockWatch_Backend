33a0b76bbe0de64b8ec57c80583b40c031f16d10
vnpugh <vnp19@yahoo.com>
2023-06-06 14:09:55 -0500

    Added exception handler

---
+++ src/main/java/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = {InformationExistException.class, InformationNotFoundException.class, InvalidInputException.class, StockNotFoundException.class, WatchlistAlreadyExistsException.class, WatchListNotFoundException.class, RuntimeException.class})
+    public ResponseEntity<Object> handleInformationExistException(RuntimeException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.CONFLICT);
+    }
+}
