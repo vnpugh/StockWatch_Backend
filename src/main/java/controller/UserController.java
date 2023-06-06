b571c94adf623c031c5e11de53d47ebe33e20de7
vnpugh <vnp19@yahoo.com>
2023-06-06 14:24:23 -0500

    Added registerUser request

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -2,9 +2,12 @@ package controller;
 
 import models.User;
 import models.request.LoginRequest;
+import models.request.RegisterUserRequest;
+import models.response.LoginResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
 import repository.UserRepository;
 import service.UserService;
